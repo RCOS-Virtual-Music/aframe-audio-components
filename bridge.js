851aa37bcc0bca8c609d72224a5693488ae3efed
Dru Ellering <54593927+dru26@users.noreply.github.com>
2021-07-14 02:15:40 -0400

    Updated node-osc and socket.io

--- bridge.js
+++ bridge.js
@@ -1,28 +1,47 @@
 // To run: node bridge.js
-// make sure to install `npm install node-osc`
-// and `npm install socket.io` in the directory
+// first run in this directory:
+// `npm install socket.io@4`
+// `npm install node-osc@6`
 
+// NOTE:
+// As of node-osc 6.0.3, npm i node-osc does not properly install
+// FIX:
+// `cd ./node_modules/node-osc/`
+// `npm i rollup`
+// `npm run-script build`
+// `cd ..`
+// `cd ..`
+
+// Start
 console.log('test');
 
-var clientPort = 8081;
+var serverPort = 8081;
 
-var osc = require('node-osc');
-var io = require('socket.io').listen(clientPort);
+//import { Client, Server } from 'node-osc';
+var osc = require('node-osc')
+
+var io = require('socket.io')(serverPort, { cors:true, origins:'*'});
+//io.origins('*');
+//io.set('origins', '*');
 
 // Setup the socket
-io.on('connection', function(socket) {
+io.on('connection', socket => {
 	console.log('STARTING');
 	// On config
 	socket.on('config', function(obj) {
+		console.log('New connection detected!');
 		console.log('config', obj);
-		oscServer = new osc.Server(obj.server.port, obj.server.host);
-		oscClient = new osc.Client(obj.client.host, obj.client.port);
-		console.log('did something happen');
-		oscClient.send('/status ,is', socket.id + ' running');
-
-		oscServer.on('message', function(msg, rinfo) {
+		// Setup an OSC connection
+		this.oscServer = new osc.Server(obj.server.port, obj.server.host);
+		this.oscClient = new osc.Client(obj.client.host, obj.client.port);
+		// Send the connection a status update
+		this.oscClient.send('/oscAddress', 200, () => {
+			this.oscClient.close();
+		});
+		// Recieve future OSC messages and pass them onto the connected socket
+		this.oscServer.on('message', function(msg) {
 			socket.emit('message', msg);
-			console.log('sent OSC message to WS', msg, rinfo);
+			console.log('sent OSC message to Client', msg);
 		});
 	});
 	io.sockets.on('connection', function (socket) {
@@ -32,11 +51,12 @@ io.on('connection', function(socket) {
 	// When the server recieves a message
 	socket.on('message', function(obj) {
 		var toSend = obj.split(' ');
-		oscClient.send(...toSend);
-		console.log('sent WS message to OSC', toSend);
+		this.oscClient.send(...toSend);
+		console.log('sent Client message to OSC', toSend);
 	});
-	// When the server disconnects
+	// When the client disconnects
 	socket.on("disconnect", function () {
-		this.oscServer.kill();
+		this.oscClient.close();
+		this.oscServer.close();
 	})
 });
--- components/osc-manager.js
+++ components/osc-manager.js
@@ -1,12 +1,42 @@
 AFRAME.registerComponent('osc-manager', {
 	schema: {
-		serverID: { type: 'string', default: '127.0.0.1' },
+		serverHost: { type: 'string', default: '127.0.0.1' },
 		serverPort: { type: 'int', default: 8000 },
-		clientID: { type: 'string', default: '127.0.0.1' },
+		clientHost: { type: 'string', default: '127.0.0.1' },
 		clientPort: { type: 'int', default: 8001 },
 		id: { type: 'int', default: 0 }
 	},
 	init: function() {
-		
+		// Import io directly from the server
+		//import io from 'http://127.0.0.1:8081/socket.io/socket.io.js'
+
+		//var io = require('socket.io');
+
+		fetch('http://127.0.0.1:8081/socket.io/socket.io.js')
+			.then(response => response.text())
+			.then(text => eval(text))
+			.then(() => {
+				var socket = io('http://127.0.0.1:8081');
+				socket.on('connect', function() {
+					// sends to socket.io server the host/port of oscServer
+					// and oscClient
+					socket.emit('config',
+						{
+							server: {
+								port: this.serverPort,
+								host: this.serverHost
+							},
+							client: {
+								port: this.clientPort,
+								host: this.clientHost
+							}
+						}
+					);
+				});
+
+				socket.on('message', function(obj) {
+					console.log(obj);
+				});
+			})
 	}
 })
