e8ef9b1a0ecd45b8a2a0cb557e1c45247995b6de
Rachel Wang <wangr15@rpi.edu>
2021-08-16 22:48:38 -0400

    Revert "Merge branch 'main' of https://github.com/rachel-wg/aframe-audio-components into main"
    This reverts commit a29e3577e7f3fab034bd2118f4b564c8718416d1, reversing
    changes made to e560d982b8f49415ff31d162c80eca104d4c3a9f.

--- components/osc-receiver.js
+++ components/osc-receiver.js
@@ -1,19 +1,5 @@
 AFRAME.registerComponent('osc-receiver', {
     schema: {
         line: { type: 'int', default: 0 }
-    },
-    update: function () {
-      var data = this.data;  // Component property values.
-      var el = this.el;  // Reference to the component's entity.
-      console.log("data")
-      if (data.event) {
-        // This will log the `message` when the entity emits the `event`.
-        el.addEventListener(data.event, function () {
-          console.log(data.message);
-        });
-      } else {
-        // `event` not specified, just log the message.
-        console.log(data.message);
-      }
     }
 })
